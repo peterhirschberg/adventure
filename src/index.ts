78d5b04c917c7379b461e4de2f62c0b93ece9aae
Peter Hirschberg <email@peterhirschberg.com>
2019-08-26 03:27:27 -0400

    Added fullscreen option

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import {start} from './platform'
 import {COLOR} from './adventure'
 
+let fullScreen = false
+
 let body: any
+let heading: any
 let instructions: any
 let links: any
+let button: any
 
 document.addEventListener('DOMContentLoaded', () => { 
 
   // Grab some DOM elements
   body = document.querySelector('body')
+  heading = document.querySelector('.heading')
   instructions = document.querySelector('.instructions')
   links = document.querySelectorAll('a')
+  button = document.querySelector('button')
+
+  button.onclick = onFullScreen
 
   // Init the game
   start()
 
 }, false)
+
+function onFullScreen() {
+  body.classList.add('fullscreen')
+}
 
 export function onRoomColorChanged(color: COLOR) {
   // Make the page elements match the current room color
@@ -25,4 +37,6 @@ export function onRoomColorChanged(color: COLOR) {
   links.forEach((link:any) => {
     link.style.color = colorstr
   })
+  button.style.color = colorstr
+  button.style.border = '2px solid ' + colorstr
 }
